58bbdee7695394478ee71adc0925c13a7b4564a4
Adi Rizka <26130158+adirizka7@users.noreply.github.com>
2021-08-30 07:31:27 +0700

    add input page layout

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,9 +12,56 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ), // AppBar
-      body: Center(
-        child: Text('Body Text'),
-      ), // Center
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableCard(color: Color(0xFF1D1E33)),
+                  ),
+                  Expanded(
+                    child: ReusableCard(color: Color(0xFF1D1E33)),
+                  ),
+                ],
+              ), // Row
+            ), // Expanded
+            Expanded(
+              child: ReusableCard(color: Color(0xFF1D1E33)),
+            ), // Expanded
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableCard(color: Color(0xFF1D1E33)),
+                  ),
+                  Expanded(
+                    child: ReusableCard(color: Color(0xFF1D1E33)),
+                  ),
+                ],
+              ), // Row
+            ), // Expanded
+          ],
+        ), // Column
+      ), // SafeArea
     ); // Scaffold
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.color});
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
+      ), // BoxDecoration
+    ); // Container
   }
 }
